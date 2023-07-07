10a51053cf35385998348cd771ed03f6a86d9428
VaBir <102629477+VaBir@users.noreply.github.com>
2023-07-07 13:00:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,21 +29,7 @@ while play_again != 'q':
 
 
 
-  for attempt in range(1, 7):
-    guess = input().lower()
-  
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-    
-    for i in range( min(len(guess), 5)):
-      if guess[i] == word[i]:
-        print(colored(guess[i], 'green'), end = '')
-      elif guess[i] in word:
-        print(colored(guess[i], 'yellow'), end = '')
-      else:
-        print(guess[i], end = '')
-    
-    print()
+
     
     if guess == word:
         print(colored(f'Congratulations!! You found the word in {attempt} !!!!!!', 'red'))
